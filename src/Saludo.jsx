e7f63ddb4454ca836bce283a674e02fb03333bfe
herminia salomon victoriano <isc17350054@gmail.com>
2022-02-24 17:11:02 -0600

    hola mundo

---
+++ src/Saludo.jsx
@@ -0,0 +1,4 @@
+const Saludo = ()  => { 
+    return <h1> hola mundo otra vez</h1>; 
+};
+export default Saludo;
---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import Saludo from "./Saludo"
+
+function app() {
+    return (
+        <div className="App">
+           <Saludo />
+        </div>
+    );
+}
+
+export default App;
